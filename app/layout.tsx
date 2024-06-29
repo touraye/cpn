b91168bf61ac406d6285f94ef448be3bf2f1e742
touraye <touraye07@gmail>
2024-06-29 15:53:15 +0000

    setup theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,34 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/theme-provider'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Cash Power App",
-  description: "Purchase your cash power with ease, anywhere, at anytime",
-};
+	title: 'Cash Power App',
+	description: 'Purchase your cash power with ease, anywhere, at anytime',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <ClerkProvider>      
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={inter.className}>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='system'
+						enableSystem
+						disableTransitionOnChange>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	)
 }
